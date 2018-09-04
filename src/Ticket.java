711484f91b80d63dc4ac28abcacb6cd30e527725
ieva.kangsepa <ievk@itu.dk>
2018-09-04 18:04:56 +0200

    Initial commit

---
+++ src/Ticket.java
@@ -0,0 +1,13 @@
+public class Ticket {
+
+
+    private int price;
+
+    private String venue ;
+
+
+    public Ticket(int price, String venue) {
+        this.price = price;
+        this.venue = venue;
+    }
+}
